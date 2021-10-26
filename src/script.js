cf98dc453a706b73b8e0616078a09e7816f1152f
Jhonata-Jackson <jhonata.jackson@sga.pucminas.br>
2021-10-25 22:57:35 -0300

    Move o código de scritp para um arquivo externo

--- src/script.js
+++ src/script.js
@@ -1,7 +1,26 @@
+function getUserData() {
+    let email = document.getElementById('exampleInputEmail1').value
+    let password = document.getElementById('exampleInputPassword1').value
+    let userData = {
+        email,
+        password
+    }
+    if(email && password) {
+        alert('CADASTRADO COM SUCESSO!!!')
+        console.log(userData)
+    } else {
+        alert('PREENCHA TODOS OS CAMPOS!!!')
+    }
+}
 
-function teste() {
-    document.querySelector("form")
-    .addEventListener("submit", () => console.log('Dados enviado com sucesso'))
-
-    return false
+window.onload = function () {
+var div = document.getElementById('exampleCheck1')
+    div.addEventListener('click', function () {
+        var senha = document.getElementById("exampleInputPassword1")
+        if(senha && senha.type === "password") {
+            senha.type = "text"
+        } else {
+            senha.type = "password"
+        }
+    }, false)
 }
