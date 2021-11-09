9bd35a07ca83ade47944822cea842dfe711fade9
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2021-11-09 20:57:14 -0300

    Implementa materias de tecnologia

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,9 +1,9 @@
 function getData() {
     return {
         categorias: {
-            geek: [
+            tecnologia: [
                 {
-                    id: "gk1",
+                    id: "tec1",
                     title: "NVIDIA promete revolucionar conceito de metaverso com avatares tecnológicos",
                     description: "A corrida pelo desenvolvimento no novo mundo digital começou",
                     font: "https://canaltech.com.br/",
@@ -13,7 +13,7 @@ function getData() {
 
                 },
                 {
-                    id: "gk2",
+                    id: "tec2",
                     title: "Microsoft anuncia Windows 11 SE, focado no mercado educacional",
                     description: "Sistema operacional voltado para o mercado educacional.",
                     font: "https://www.tecmundo.com.br/",
@@ -23,73 +23,73 @@ function getData() {
 
                 },
                 {
-                    id: "gk3",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec3",
+                    title: "5 sites que oferecem cursos gratuitos de tecnologia",
+                    description: "Conheça 5 sites que oferecem cursos gratuitos de tecnologia",
+                    font: "https://tecnoblog.net/",
+                    link: "https://tecnoblog.net/529961/5-sites-que-oferecem-cursos-gratuitos-de-tecnologia/",
                     img: "",
-                    content: ""
+                    content: "Com o mercado de trabalho sempre exigindo currículos cada vez mais completos, cabe à pessoa se desenvolver e se aprimorar diariamente. Dessa forma, é interessante conhecer sites que oferecem cursos gratuitos de tecnologia para garantir o ganho de conhecimento constante. Tudo fica ainda mais interessante se podem ser realizados no conforto de seu lar. Nesta lista, separei cinco opções variadas. Confira!"
 
                 },
                 {
-                    id: "gk4",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec4",
+                    title: "Professor da UnB recebe prêmio de inovação",
+                    description: "Nanotecnologia na agricultura",
+                    font: "https://g1.globo.com/",
+                    link: "https://g1.globo.com/df/distrito-federal/noticia/2021/11/08/professor-da-unb-recebe-premio-de-inovacao-por-biofertilizante-que-usa-nanotecnologia-na-agricultura.ghtml",
                     img: "",
-                    content: ""
+                    content: "Brenno, de 43 anos, explica que o produto que leva o nome da UnB à posição de destaque no evento nacional usa nanotecnologia – partículas pequenas que não são visíveis a olho nu – aplicada à agricultura. Formado em química, o pesquisador é pós-doutor em biotecnologia molecular e celular."
 
                 },
                 {
-                    id: "gk5",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec5",
+                    title: "Bitcoin bate seu preço histórico e alcança US$ 68 mil",
+                    description: "Bitcons finalmente alcançou uma nova alta histórica",
+                    font: "https://www.tecmundo.com.br/",
+                    link: "https://www.tecmundo.com.br/mercado/228361-bitcoin-bate-preco-historico-alcanca-us-68-mil.htm",
                     img: "",
-                    content: ""
+                    content: "Em forte movimento de alta, o Bitcoin (BTC) retornou ao patamar dos US$ 66 mil por unidade no começo da semana e finalmente alcançou uma nova alta histórica, chegando em US$ 68 mil nesta terça-feira. A repentina valorização, que começou ainda na segunda-feira (8), também impulsionou algumas das principais criptomoedas alternativas, ou altcoins, como a Cardano (ADA) e Ether (ETH), o token nativo da rede Ethereum, que já está valendo US$ 4,8 mil."
 
                 },
                 {
-                    id: "gk6",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec6",
+                    title: "O que é blockchain? ",
+                    description: "Conheça a tecnologia que torna as transações com criptos possíveis",
+                    font: "https://www.infomoney.com.br/",
+                    link: "https://www.infomoney.com.br/guias/blockchain/",
                     img: "",
-                    content: ""
+                    content: "A blockchain nasceu com o Bitcoin (BTC) e continua sendo um dos pilares da primeira criptomoeda do mundo. Com o tempo, no entanto, também tomou vida própria e começou a andar sozinha, explorando outros mercados."
 
                 },
                 {
-                    id: "gk7",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec7",
+                    title: "Brasil está entre os dez países com maior quantidade de cibercrimes no mundo",
+                    description: "Crime digital é uma ameaça da nova era",
+                    font: "https://www.tudocelular.com/",
+                    link: "https://www.tudocelular.com/seguranca/noticias/n181432/brasil-maior-quantidade-cibercrimes-mundo.html",
                     img: "",
-                    content: ""
+                    content: "Um relatório da empresa de segurança digital NSFOCUS colocou o Brasil em nono lugar na lista das dez nações mais atuantes em cibercrimes no mundo. Os dados revelam que o país saltou da 15ª posição em 2019 para figurar entre os líderes de ataques hacker, na frente do Reino Unido e atrás da Alemanha."
 
                 },
                 {
-                    id: "gk8",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec8",
+                    title: "Nubank libera cadastro do NuSócios",
+                    description: "O Nubank começou a permitir o cadastro de milhões de clientes selecionados no programa NuSócios",
+                    font: "https://tecnoblog.net/",
+                    link: "https://tecnoblog.net/530687/nubank-libera-cadastro-do-nusocios-para-receber-acao-de-graca-apos-ipo/",
                     img: "",
-                    content: ""
+                    content: "A partir desta terça-feira (9), clientes do Nubank já podem se cadastrar para receber seu “pedacinho” da fintech de graça. O programa NuSócios foi lançado hoje pelo banco digital para oferecer um BDR, título equivalente a uma fração de ação negociada na Bolsa de Valores, a milhões de usuários selecionados. Se você é uma dessas pessoas, um grande banner vai aparecer no topo da tela inicial do aplicativo do Nubank."
 
                 },
                 {
-                    id: "gk9",
-                    title: "",
-                    description: "",
-                    font: "",
-                    link: "",
+                    id: "tec9",
+                    title: "Clientes do Banco Inter reclamam de erro",
+                    description: "Banco Inter prometeu cartão Black a quem cumprisse todas as missões de sua Inter Week de investimentos",
+                    font: "https://tecnoblog.net/",
+                    link: "https://tecnoblog.net/530194/clientes-do-banco-inter-reclamam-de-erro-em-promocao-que-dava-cartao-black/",
                     img: "",
-                    content: ""
+                    content: "O Banco Inter faz anualmente um evento chamado Inter Week, uma semana com uma série de lives sobre investimentos. O maior chamariz, porém, é a promoção que dá cashback em compras ou um upgrade de cartão de crédito. Para ganhar, o cliente precisa aplicar seu dinheiro em produtos distribuídos pela empresa — cada operação gera pontos, e o prêmio depende do placar final. Só que muita gente está reclamado que a companhia não computou corretamente sua participação."
 
                 },
               
@@ -116,7 +116,7 @@ function getData() {
                 },
 
             ],
-            tecnologia: [
+            geek: [
                 {
                     id: "tc1",
                     title: "",
