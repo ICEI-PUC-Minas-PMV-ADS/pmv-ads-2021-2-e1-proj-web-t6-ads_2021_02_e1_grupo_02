582353863f21bfdf0fd9622cb1e499259e5d9a9f
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2021-11-09 19:59:45 -0300

    implementa os dados das matérias

---
+++ src/assets/data.js
@@ -0,0 +1,163 @@
+function getData() {
+    return {
+        categorias: {
+            geek: [
+                {
+                    id: "gk1",
+                    title: "NVIDIA promete revolucionar conceito de metaverso com avatares tecnológicos",
+                    description: "A corrida pelo desenvolvimento no novo mundo digital começou",
+                    font: "https://canaltech.com.br/",
+                    link: "https://canaltech.com.br/inovacao/nvidia-promete-revolucionar-conceito-de-metaverso-com-avatares-tecnologicos-201269/",
+                    img: "",
+                    content: "A NVIDIA anunciou nesta terça-feira (9) o lançamento da sua plataforma tecnológica para criação de avatares interativos baseados em inteligência artificial. O Omniverse Avatar pretende entregar personagens altamente detalhados com gráficos 3D e Ray Tracing que poderão ver, falar, conversar e interagir com pessoas de forma natural."
+
+                },
+                {
+                    id: "gk2",
+                    title: "Microsoft anuncia Windows 11 SE, focado no mercado educacional",
+                    description: "Sistema operacional voltado para o mercado educacional.",
+                    font: "https://www.tecmundo.com.br/",
+                    link: "https://www.tecmundo.com.br/software/228405-microsoft-anuncia-windows-11-focado-mercado-educacional.htm",
+                    img: "",
+                    content: "A Microsoft anunciou nesta terça-feira (9) o lançamento do Windows 11 SE, sistema operacional voltado para o mercado educacional. Focado especialmente para estudantes do ensino fundamental e médio, Windows 11 SE deve ser mais intuitivo e fácil de mexer. Segundo a empresa, o SO foi projetado para atender às observações de professores e profissionais de TI das escolas."
+
+                },
+                {
+                    id: "gk3",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "gk4",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "gk5",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "gk6",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "gk7",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "gk8",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "gk9",
+                    title: "",
+                    description: "",
+                    font: "",
+                    link: "",
+                    img: "",
+                    content: ""
+
+                },
+              
+
+            ],
+            ciencia: [
+                {
+                    id: "cc1",
+                    title: "",
+                    description: "",
+                    font: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "cc2",
+                    title: "",
+                    description: "",
+                    font: "",
+                    img: "",
+                    content: ""
+
+                },
+
+            ],
+            tecnologia: [
+                {
+                    id: "tc1",
+                    title: "",
+                    description: "",
+                    font: "",
+                    img: "",
+                    content: ""
+
+                },
+                {
+                    id: "tc2",
+                    title: "",
+                    description: "",
+                    font: "",
+                    img: "",
+                    content: ""
+
+                },
+            ],
+            // podcast: [
+            //     {
+            //         id: "pd1",
+            //         title: "",
+            //         description: "",
+            //         font: "",
+            //         img: "",
+            //         content: ""
+
+            //     },
+            //     {
+            //         id: "pd2",
+            //         title: "",
+            //         description: "",
+            //         font: "",
+            //         img: "",
+            //         content: ""
+
+            //     },
+            // ]
+        }
+    }
+}
+
+
