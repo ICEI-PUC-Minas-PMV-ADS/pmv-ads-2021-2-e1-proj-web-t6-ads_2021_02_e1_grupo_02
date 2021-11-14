b5cc43d78b8804458f028aab77233d09378ba127
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2021-11-14 09:13:56 -0300

    Implementa dados das matérias de ciência

--- src/assets/data.js
+++ src/assets/data.js
@@ -98,20 +98,92 @@ function getData() {
             ciencia: [
                 {
                     id: "cc1",
-                    title: "",
-                    description: "",
-                    font: "",
+                    title: "O céu não é o limite",
+                    description: "Eclipse lunar vem aí, tem asteroide por perto e mais!",
+                    font: "https://canaltech.com.br/",
+                    link: "https://canaltech.com.br/ciencia/o-ceu-nao-e-o-limite-eclipse-lunar-vem-ai-tem-asteroide-por-perto-e-mais-201705/",
                     img: "",
-                    content: ""
+                    content: "O mais longo eclipse lunar deste século vai acontecer na próxima semana, mas já estamos nos preparando para as observações — e já você pode conferir os detalhes sobre o evento nas próximas linhas. Além disso, receberemos uma visitinha de um asteroide que passará perto da Terra em breve."
 
                 },
                 {
                     id: "cc2",
-                    title: "",
-                    description: "",
-                    font: "",
+                    title: "Teoria de Eisten Calibra GPS",
+                    description: "Como teoria da relatividade de Einstein calibra GPS e traduz o Universo... - Veja mais em https://www.uol.com.br/tilt/noticias/redacao/2021/11/14/e--mc2-teoria-da-relatividade-calibra-gps-e-traduz-o-universo.htm?cmpid=copiaecola",
+                    font: "https://www.uol.com.br/",
+                    link: "https://www.uol.com.br/tilt/noticias/redacao/2021/11/14/e--mc2-teoria-da-relatividade-calibra-gps-e-traduz-o-universo.htm",
                     img: "",
-                    content: ""
+                    content: "A gente sabe bem que Einstein foi importantíssimo e que a sua teoria da relatividade, aquela do E = mc2, é fundamental para muita coisa. Mas explicar exatamente o que significa isso já complica um pouco. Pois é essa teoria aí, publicada há mais de 117 anos, que calibra o GPS e faz com que os motoristas não se percam por aí.... - Veja mais em https://www.uol.com.br/tilt/noticias/redacao/2021/11/14/e--mc2-teoria-da-relatividade-calibra-gps-e-traduz-o-universo.htm?cmpid=copiaecola"
+
+                },
+                {
+                    id: "cc3",
+                    title: "Objeto estranho próximo à ISS",
+                    description: "Astronautas da SpaceX Crew-3 viram objeto estranho próximo à ISS",
+                    font: "https://olhardigital.com.br/",
+                    link: "https://olhardigital.com.br/2021/11/12/ciencia-e-espaco/astronautas-da-spacex-crew-3-viram-objeto-estranho-proximo-a-iss/",
+                    img: "",
+                    content: "Enquanto se aproximavam da Estação Espacial Internacional (ISS), os astronautas da missão SpaceX Crew-3, que foi lançada pela Nasa na última quarta-feira (10), avistaram um estranho objeto flutuando no espaço."
+
+                },
+                {
+                    id: "cc4",
+                    title: "Robô da NASA faz descoberta inédita",
+                    description: "Descoberta incrível ao investigar rochas em Marte",
+                    font: "https://br.financas.yahoo.com/",
+                    link: "https://br.financas.yahoo.com/noticias/robo-da-nasa-faz-descoberta-inedita-ao-investigar-rochas-em-marte-173251992.html",
+                    img: "",
+                    content: "O Perseverance está investigando as rochas da região sul de Marte desde 25 de outubro, local de interesse da equipe de cientistas. O robô tem um mecanismo no braço robótico que permite raspar camada do material, de forma a enxergar o que há dentro de sua solidez."
+
+                },
+                {
+                    id: "cc5",
+                    title: "Espécie de dinossauro com nariz exótico é descoberta na Inglaterra",
+                    description: "Animal viveu na Terra há 125 milhões de anos, tinha 8 metros de comprimento e pesava aproximadamente 600 kg",
+                    font: "https://noticias.r7.com/",
+                    link: "https://noticias.r7.com/tecnologia-e-ciencia/especie-de-dinossauro-com-nariz-exotico-e-descoberta-na-inglaterra-11112021",
+                    img: "",
+                    content: "Um cientista que faz parte do programa de doutorado da Universidade de Portsmouth, localizada em Londres, no Reino Unido, descobriu uma espécie de dinossauro com uma característica um tanto quanto diferente: um focinho extremamente grande."
+
+                },
+                {
+                    id: "cc6",
+                    title: "Tudo sobre Aurora Borel",
+                    description: "O que é aurora boreal e onde ver o fenômeno no céu?",
+                    font: "https://canaltech.com.br/",
+                    link: "https://canaltech.com.br/espaco/o-que-e-aurora-boreal-201584/",
+                    img: "",
+                    content: "A aurora boreal é um fenômeno bastante impressionante, proporcionando um verdadeiro espetáculo de luzes coloridas no céu. Vistas da superfície as auroras já deixam qualquer observador boquiaberto e, quando vistas do espaço, são ainda mais espetaculares. Mas, afinal, o que é aurora boreal? Como se forma uma aurora boreal? E onde tem aurora boreal acontecendo em nosso planeta? É o que você descobre nesta matéria!"
+
+                },
+                {
+                    id: "cc7",
+                    title: "Covid psicológico",
+                    description: "Doença psicológica pode causar sintomas persistentes, diz estudo.",
+                    font: "https://www.uol.com.br/",
+                    link: "https://www.uol.com.br/vivabem/noticias/rfi/2021/11/13/covid-psicologico-poderia-causar-sintomas-persistentes-diz-estudo.htm",
+                    img: "",
+                    content: "A covid persistente seria principalmente de ordem psicológica? A sugestão feita por um estudo recente em um hospital de Paris indignou pacientes e recebeu inúmeras críticas científicas pela metodologia utilizada, destacando os inúmeros mistérios que ainda cercam esta doença."
+
+                },
+                {
+                    id: "cc8",
+                    title: "Pesquisa de moscas de frutas na ONU",
+                    description: "Conheça a cientista baiana que pesquisa moscas de fruta na ONU",
+                    font: "https://www.correio24horas.com.br/",
+                    link: "https://www.correio24horas.com.br/noticia/nid/conheca-a-cientista-baiana-que-pesquisa-moscas-de-fruta-na-onu/",
+                    img: "",
+                    content: "A bióloga e pesquisadora baiana Vanessa Dias, 36 anos, sempre soube que não poderia desperdiçar oportunidades. Foi assim quando decidiu trabalhar no McDonald’s, ainda no final do Ensino Médio, para conseguir pagar o cursinho pré-vestibular e quando se tornou bolsista de iniciação científica na Universidade Federal da Bahia (Ufba)."
+
+                },
+                {
+                    id: "cc9",
+                    title: "Tudo sobre como se termina uma pandemia",
+                    description: "Como pandemias chegam ao fim, segundo a ciência",
+                    font: "https://gizmodo.uol.com.br/",
+                    link: "https://gizmodo.uol.com.br/como-pandemias-chegam-ao-fim-segundo-a-ciencia/",
+                    img: "",
+                    content: "Nas últimas semanas, algumas cidades brasileiras têm apresentado avanços na contenção dos casos de Covid-19 – graças à vacinação. E, ainda que a notícia seja animadora e você esteja pensando no ‘fim da pandemia’, alguns especialistas apontam que o momento é de cautela."
 
                 },
 
